3a0f1fafe14403de0a6f8d12b7355f19a3c9302d
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-11-21 09:07:30 +0100

    Crear Proyecto

---
+++ Ejercicio_Evaluacion/src/main/java/com/salesianostriana/dam/ejercicioevaluacion/EjercicioEvaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejercicioevaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioEvaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioEvaluacionApplication.class, args);
+	}
+
+}
---
+++ Ejercicio_Evaluacion/src/test/java/com/salesianostriana/dam/ejercicioevaluacion/EjercicioEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejercicioevaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
